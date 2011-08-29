2d70cf14ed25d0e408c06de90c507fb00951641a
claudio <claudio@c91c8a1e-5377-bf51-ba6b-6081f44fa8ae>
2011-08-29 10:24:16 +0000

    aggiunto progetto DataMining
    A    DataMining
    A    DataMining/.classpath
    A    DataMining/.project
    A    DataMining/src
    A    DataMining/src/classification
    A    DataMining/src/classification/TicTacToeLoader.java
    A    DataMining/src/classification/GenericHunt.java
    A    DataMining/data
    A    DataMining/data/tic-tac-toe
    A    DataMining/data/tic-tac-toe/tic-tac-toe.data
    A    DataMining/data/tic-tac-toe/tic-tac-toe.names
    A    DataMining/.settings
    A    DataMining/.settings/org.eclipse.jdt.core.prefs

---
+++ progetti/DataMining/src/classification/GenericHunt.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package classification;
+
+/**
+ * @author Claudio Tanci
+ *
+ */
+public class GenericHunt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
